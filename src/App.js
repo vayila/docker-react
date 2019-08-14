2df3e2f011e801a41fc7b7c3f16210940146ed74
vayila <venuvayila@gmail.com>
2019-08-15 02:54:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hi Venu Welcome to your first App for second time
+          Hi Venu Welcome back
         </p>
       </div>
     );
